60dfa10983f7d102c1f9575bf23c82c3b7e583f0
Algirdas <algirdas@zyro.com>
2020-11-15 01:22:03 +0200

    Disable logo from header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header class="header">
     <div class="header__container container">
       <div class="header__actions">
-        <AppLogo class="header__logo" />
+        <!-- <AppLogo class="header__logo" /> -->
         <nav class="header__nav" v-if="nav">
           <NuxtLink class="header__link" v-for="{ page, label } in nav" v-text="label" :to="`/${page}`" :key="page" />
         </nav>
@@ -27,7 +27,7 @@
 </template>
 
 <script>
-import AppLogo from "@/components/AppLogo";
+// import AppLogo from "@/components/AppLogo";
 import AppBtn from "@/components/AppBtn";
 import IconEmail from "@/components/icons/IconEmail";
 import IconPhone from "@/components/icons/IconPhone";

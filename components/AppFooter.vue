ce53feec6889a36aa203e6f5fef4002adf3f0906
Algirdas <algirdas@zyro.com>
2020-11-15 01:45:46 +0200

    Bring logos back

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="footer__container container">
       <div class="footer__copyright">
-        <!-- <AppLogo class="footer__logo" /> -->
+        <AppLogo class="footer__logo" />
         <div class="footer__footnote" v-text="footnote" />
       </div>
       <div class="footer__socials socials">
@@ -16,11 +16,11 @@
 </template>
 
 <script>
-// import AppLogo from "@/components/AppLogo";
+import AppLogo from "@/components/AppLogo";
 
 export default {
   components: {
-    // AppLogo,
+    AppLogo,
   },
   props: {
     footer: {

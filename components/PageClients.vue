f116a92763886ded6fb8eccbfcd65b0f53b96acd
Algirdas <algirdas@zyro.com>
2020-11-15 16:58:52 +0200

    Split clients into components

---
+++ components/PageClients.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="clients">
+    <div class="clients__grid">
+      <div class="clients__block" v-for="{ logo, title, body, url } in clients" :key="title">
+        <a :href="url" class="clients__logo" target="_blank" v-if="logo">
+          <img :src="logo" :alt="title" class="clients__image" />
+        </a>
+        <div class="clients__text" v-if="body">{{ body }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageIntro from "@/components/PageIntro";
+
+export default {
+  props: {
+    clients: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss">
+.clients {
+  &__grid {
+    display: grid;
+    grid-gap: 0 3rem;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto;
+    padding: 8.1rem 0;
+  }
+  &__block {
+    padding: 2.5rem 0;
+  }
+  &__logo {
+    position: relative;
+    display: inline-block;
+  }
+  &__image {
+    width: 19rem;
+    height: 3rem;
+    object-fit: contain;
+    object-position: 0 50%;
+  }
+  &__link {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  &__text {
+    padding: 1.5rem 0;
+    min-height: 10.4rem;
+    max-width: 30.4rem;
+    line-height: 1.33;
+  }
+}
+</style>
--- pages/clients.vue
+++ pages/clients.vue
@@ -2,74 +2,30 @@
   <div class="page page--clients">
     <div class="page__container container">
       <PageIntro class="page__intro" :document="document" />
-      <div class="page__clients clients">
-        <div class="clients__grid">
-          <div class="clients__block" v-for="{ logo, title, body, url } in clients" :key="title">
-            <a :href="url" class="clients__logo" target="_blank" v-if="logo">
-              <img :src="logo" :alt="title" class="clients__image" />
-            </a>
-            <div class="clients__text" v-if="body">{{ body }}</div>
-          </div>
-        </div>
-      </div>
+      <PageClients class="page__clients" :clients="clients" />
     </div>
   </div>
 </template>
 
 <script>
 import PageIntro from "@/components/PageIntro";
+import PageClients from "@/components/PageClients";
 
 export default {
   components: {
-    PageIntro,
+    PageClients,
   },
   async asyncData({ $content, store }) {
     const document = await $content("clients").fetch();
-    const { slug, title, blocks } = document;
+    const { blocks } = document;
 
     store.commit("setBlobColors", {
       leftBlobColor: document.leftBlobColor,
       rightBlobColor: document.rightBlobColor,
     });
 
-    return { slug, title, document, clients: document.items };
+    return { document, clients: document.items };
   },
 };
 </script>
-<style lang="scss">
-.clients {
-  &__grid {
-    display: grid;
-    grid-gap: 0 3rem;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: auto;
-    padding: 8.1rem 0;
-  }
-  &__block {
-    padding: 2.5rem 0;
-  }
-  &__logo {
-    position: relative;
-    display: inline-block;
-  }
-  &__image {
-    width: 19rem;
-    height: 3rem;
-    object-fit: contain;
-    object-position: 0 50%;
-  }
-  &__link {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
-  &__text {
-    padding: 1.5rem 0;
-    min-height: 10.4rem;
-    max-width: 30.4rem;
-    line-height: 1.33;
-  }
-}
-</style>
+

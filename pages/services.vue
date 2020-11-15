e6e83ee2cb1d37555aed8c117112894ad4c045e4
Algirdas <algirdas@zyro.com>
2020-11-15 07:51:35 +0200

    Workin services btn POC

--- pages/services.vue
+++ pages/services.vue
@@ -2,6 +2,21 @@
   <div class="page page--services">
     <PageIntro class="page__intro" :slug="slug" :title="title" :document="document" />
     <div class="page__services services">
+      <div class="page__container container">
+        <div class="block">
+          <div class="block__wrapper">
+            <div class="block__base">
+              <div class="block__media">
+                <img src="/images/uploads/passport-thumb.png" alt="" class="block__image" />
+              </div>
+              <div class="block__rect">
+                <i class="block__line block__line--left"></i>
+                <i class="block__line block__line--top"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="services__grid">
         <div class="services__block" v-for="{ logo, title, body, url } in services" :key="title">
           <div class="services__logo" v-if="logo">
@@ -32,12 +47,6 @@ export default {
     });
 
     return { slug, title, document, services: document.items };
-  },
-  mounted() {
-    this.$emit("set-blobs-colors", {
-      leftBlobColor: document.leftBlobColor,
-      rightBlobColor: document.rightBlobColor,
-    });
   },
 };
 </script>

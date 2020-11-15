aff904eeb2fb5ebce43b1a4c089df2a4d31c057c
Algirdas <algirdas@zyro.com>
2020-11-15 09:02:52 +0200

    Restructure extrudes

--- pages/services.vue
+++ pages/services.vue
@@ -7,6 +7,7 @@
           <div class="services__step step" v-for="{ title, body } in steps" :key="title">
             <div class="step__top">
               <div class="step__title" v-text="title" />
+              <IconArrow :class="['step__arrow', `step__arrow--${'active'}`]" />
             </div>
             <div class="step__text" v-text="body" />
           </div>
@@ -16,6 +17,7 @@
             <div class="extrude__wrapper">
               <div class="extrude__cropper">
                 <div class="extrude__content">
+                  {{ title }}
                   <div class="extrude__title" v-text="title" />
                   <div class="extrude__text" v-text="body" />
                   <img :src="background" :alt="title" class="extrude__image" />
@@ -28,7 +30,6 @@
             </div>
           </div>
         </div>
-        <div class="extrude"></div>
       </div>
     </div>
   </div>
@@ -36,15 +37,16 @@
 
 <script>
 import PageIntro from "@/components/PageIntro";
+import IconArrow from "@/components/icons/IconArrow";
 
 export default {
   components: {
     PageIntro,
+    IconArrow,
   },
   async asyncData({ $content, store }) {
     const document = await $content("services").fetch();
     const { cta, years, experience, steps, blocks, slug, title } = document;
-    console.log(steps);
 
     store.commit("setBlobColors", {
       leftBlobColor: document.leftBlobColor,
